f76d125e9bd287a7f3c8571e0e96789b4f9c6682
Alberto Olivi <olivialberto02@gmail.com>
2026-07-06 23:49:33 -0400

    add new ability

---
+++ src/starproject/abilities/StarResonanceAbility.java
@@ -0,0 +1,23 @@
+package starproject.abilities;
+
+import com.fs.starfarer.api.impl.campaign.abilities.BaseDurationAbility;
+
+public class StarResonanceAbility extends BaseDurationAbility {
+
+    @Override
+    protected void activateImpl() {
+    }
+
+    @Override
+    protected void applyEffect(float amount, float level) {
+    }
+
+    @Override
+    protected void deactivateImpl() {
+    }
+
+    @Override
+    public boolean isUsable() {
+        return true; // We'll replace this next
+    }
+}
